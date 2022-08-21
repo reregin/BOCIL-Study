56e554a4f2bbb7ee7496f1fb053c18fe7c24c9f7
reregin <89380352+reregin@users.noreply.github.com>
2022-08-20 20:27:49 -0700

    Add files via upload

---
+++ BOCIL Study/script.js
@@ -0,0 +1,11 @@
+/*.audio-alphabet img{
+  document.getElementById("playbutton").onclick = function() {
+    var audio = document.getElementById("alphabet-audio");
+    if (audio.paused == false) {
+      audio.pause();
+    }
+    else {
+      audio.play();
+};
+}*/
+
